44a7b59b7062e9271e9e5452dc6723c553b8a4a8
Anubhav Saini <anubhav.21b1531122@abes.ac.in>
2025-07-20 17:24:00 +0530

    Button Update

--- portfolio3/src/components/aboutMeSection/AboutMeText.jsx
+++ portfolio3/src/components/aboutMeSection/AboutMeText.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
 const AboutMeText = () => {
+   const handleClick = () => {
+    const contactSection = document.getElementById('projects');
+    if (contactSection) {
+      contactSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
   return (
     <div className="flex flex-col md:item-start sm:item-center md:text-left sm:text-center">
       <h2 className="text-6xl text-cyan mb-10">About Me</h2>
@@ -9,7 +15,7 @@ const AboutMeText = () => {
 
 I enjoy building responsive, user-friendly web applications and take pride in delivering intuitive UI and smooth functionality. I'm passionate about continuous learning and always eager to take on new challenges that help me grow as a developer.
       </p>
-      <button className="border border-orange rounded-full py-2 px-4 text-lg flex items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center text-white hover:text-cyan">My Project</button>
+      <button  onClick={handleClick} className="border border-orange rounded-full py-2 px-4 text-lg flex items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center text-white hover:text-cyan">My Project</button>
     </div>
   );
 };
